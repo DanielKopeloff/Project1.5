bfdda8758f7142e65507867d3a400a8d5533bfba
John Howland Stone <john.howland.stone@gmail.com>
2021-04-03 17:22:03 -0600

    Fixes an issue with email validation

--- src/main/java/StoneKopeloffProject/utililty/Validation.java
+++ src/main/java/StoneKopeloffProject/utililty/Validation.java
@@ -3,6 +3,7 @@ package StoneKopeloffProject.utililty;
 public class Validation {
     /**
      * Method that checks if a string contains valid characters for a name
+     *
      * @param str the string to check
      * @return true if its valid, false otherwise
      */
@@ -20,12 +21,16 @@ public class Validation {
 
     /**
      * Method that checks if an email address is valid
+     *
      * @param str the email address
      * @return true if its valid false otherwise
      */
-    //credit http://emailregex.com/ for the regex used below
+    //credit https://www.geeksforgeeks.org/check-email-address-valid-not-java/ for the regex used below
     public static boolean validateEmail(String str) {
-        if (str.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
+        if (str.matches("^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$")) {
             return true;
         }
         return false;
